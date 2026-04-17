3add6807295fae930e41382199e305dad35ccec4
cpaci-tani <cpacitanimulli@gmail.com>
2026-04-16 20:36:20 -0500

    feat(web/charts): barrel export

---
+++ engine/web/js/ui/charts/index.js
@@ -0,0 +1,4 @@
+export { getChartTheme, makeAxis } from './theme.js';
+export { UPlotChart }        from './uplot-chart.js';
+export { Sparkline }         from './sparkline.js';
+export { StackedAreaChart }  from './stacked-area.js';
